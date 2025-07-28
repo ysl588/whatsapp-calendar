8be4905988938fba26e9a223f34d47b9d2166aac
ysl588 <lskris58@gmail.com>
2025-07-28 21:39:39 +0800

    Add files via upload

--- whatsapp_webhook.py
+++ whatsapp_webhook.py
@@ -118,5 +118,8 @@ def whatsapp_webhook():
     resp.message(reply)
     return str(resp)
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
